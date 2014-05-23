b91d0926929f68372b500493915d512b5d984df3
bbudd <bob.budd@gmail.com>
2014-05-23 06:18:08 -0700

    initial commit
    files and setup

---
+++ app/scripts/init.js
@@ -0,0 +1,18 @@
+(function() {
+
+  'use strict';
+
+  require.config({
+    paths: {
+      underscore : 'app/vendor/underscore',
+      backbone : 'app/vendor/backbone',
+      jquery : 'app/vendor/jquery'
+    },
+
+  require([ 'app/scripts/app' ], function(app) {
+
+  });
+
+}());
+
+
